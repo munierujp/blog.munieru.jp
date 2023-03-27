86f20f1e9a432a7ffa59ae06779fa0cd649352f2
Munieru <20086673+munierujp@users.noreply.github.com>
2023-03-28 00:48:36 +0900

    Replace `.markuplintrc.json` with `markuplint.config.js` (#231)

---
+++ markuplint.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/** @typedef {import('@markuplint/ml-config').Config} Config */
+
+/**
+ * @type {Config}
+ * @see https://markuplint.dev/docs/configuration
+ */
+const config = {
+  extends: [
+    'markuplint:recommended'
+  ],
+  excludeFiles: [
+    './node_modules/**/*'
+  ]
+}
+
+module.exports = config
