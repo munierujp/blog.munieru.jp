8b668c1a9dbb1fd1f36883087b28c70107d413f9
Munieru <20086673+munierujp@users.noreply.github.com>
2023-03-28 01:13:47 +0900

    Replace `.eslintrc.json` with `.eslintrc.js` (#232)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+/** @typedef {import('eslint/lib/shared/types').ConfigData} ConfigData */
+
+/**
+ * @type {ConfigData}
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ */
+const config = {
+  extends: '@munierujp',
+  env: {
+    browser: true,
+    jquery: true
+  },
+  plugins: [
+    'html'
+  ],
+  globals: {
+    ScrollHint: 'readonly'
+  }
+}
+
+module.exports = config
