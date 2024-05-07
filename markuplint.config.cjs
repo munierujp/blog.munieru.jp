09a928abaec02bea3842a76d76c5240b9f80b1fa
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-07 22:11:30 +0900

    Migrate to ESM (#350)
    * Add `"type": "module"`
    
    * Migrate ESLint config to ESM
    
    * Migrate Stylelint config to ESM
    
    * Migrate Markuplint config to ESM

--- markuplint.config.cjs
+++ markuplint.config.cjs
@@ -7,13 +7,10 @@
  * @see https://markuplint.dev/docs/configuration
  */
 const config = {
-  extends: [
-    'markuplint:recommended'
-  ],
+  extends: 'markuplint:recommended',
   excludeFiles: [
     './node_modules/**/*'
   ]
 }
 
-// eslint-disable-next-line unicorn/prefer-module
 module.exports = config
